ae9024f8d333e00c4fa173683417b3cf0f777a9b
Gabriel Sajfar <103059888+gabsaj@users.noreply.github.com>
2022-06-09 14:13:28 +0100

    basic setup finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app/styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <img className="img--logo--small" alt="Logo-small" />
+        <button className="btn btn--nav">
+          <i className="icon icon--base icon--add"></i>
+          <div>Add product</div>
+        </button>
+      </div>
     </div>
   );
 }
